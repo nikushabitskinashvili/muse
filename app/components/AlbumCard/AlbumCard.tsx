00f6986f119ae482fab3b4689b710370f0ef81fd
nikushabitskinashvili <130066334+nikushabitskinashvili@users.noreply.github.com>
2024-07-16 14:54:06 +0400

    fix: fix brench error

--- app/components/AlbumCard/AlbumCard.tsx
+++ app/components/AlbumCard/AlbumCard.tsx
@@ -1,6 +1,4 @@
 'use client';
-
-import { useEffect, useState } from "react";
 import styles from "./AlbumCard.module.scss";
 import Link from "next/link";
 
--- app/page.tsx
+++ app/page.tsx
@@ -5,8 +5,7 @@ import styles from "./page.module.css";
 export default function Home() {
  
   return (
-    <main className={styles.main}>    
-
+      <main className={styles.main}>    
     </main>
   );
 }
