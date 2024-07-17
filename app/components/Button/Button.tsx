5e6abd5d9f65dbdf59ac391c6e34e216c85d1f0a
bokakajilashvili <128229402+bokakajilashvili@users.noreply.github.com>
2024-07-17 14:42:06 +0400

    refactor/FBLAN-27/button

--- app/components/Button/Button.tsx
+++ app/components/Button/Button.tsx
@@ -1,30 +1,29 @@
 "use client"
-import { ReactNode } from 'react';
+
 import styles from './Button.module.scss';
-import { IconEnum } from '../Icon/Icon';
-import { colorsEnum } from '@/app/utils/colors.enum';
+import { IconEnum } from '../../utils/Icon/Icon';
+
 
 type Props = {
     title: string;
-    className?: string;
+    className: string;
     disabled?: boolean;
     Icon?: keyof typeof IconEnum;
-
     color?: string;
-    variant?: 'main' | 'play' | 'shuffle' | 'create' | 'delete' | 'cancel';
+
     onClick?: () => void
 
 }
 
 export default (props : Props) => {
     const classes = [];
-    if(props.variant == 'main') classes.push(styles.container)
-    else if(props.variant == 'play') classes.push(styles.play)
+    if(props.className == 'container') classes.push(styles.container)
+    else if(props.className == 'play') classes.push(styles.play)
     if(props.color == 'pink' ) classes.push(styles.playPink)
-    if(props.variant == 'shuffle') classes.push(styles.shuffle)
-    if(props.variant == 'create') classes.push(styles.create)
-    if(props.variant == 'delete') classes.push(styles.delete)
-    if(props.variant == 'cancel') classes.push(styles.cancel)
+    if(props.className == 'shuffle') classes.push(styles.shuffle)
+    if(props.className == 'create') classes.push(styles.create)
+    if(props.className == 'delete') classes.push(styles.delete)
+    if(props.className == 'cancel') classes.push(styles.cancel)
     
     
     return(
--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import styles from "./page.module.css";
 import Input from "./components/Input/Input";
 import Link from "next/dist/client/link";
 import Button from "./components/Button/Button";
-import { IconEnum } from "./components/Icon/Icon";
+
 import { FC } from "react";
 
 
@@ -12,6 +12,7 @@ import { FC } from "react";
 export default function Home() {
   return (
     <main className={styles.main}>
+      
       
     </main>
   );
