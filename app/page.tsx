45defb6b602fac6c6bcf460018fadf73740af27e
bokakajilashvili <128229402+bokakajilashvili@users.noreply.github.com>
2024-07-17 20:04:35 +0400

    fix/FBLIAN-27/page.tsx conflict

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,15 @@
-'use client'
+"use client";
 import Image from "next/image";
 import styles from "./page.module.css";
 import Input from "./components/Input/Input";
 import Link from "next/dist/client/link";
 
 export default function Home() {
-  return (
-    <main className={styles.main}>
-   
-      
-      
-    </main>
-  );
+  return <main className={styles.main}>
+
+  
+  
+  
+  
+  </main>;
 }
