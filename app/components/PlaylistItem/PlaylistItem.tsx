ebf52387ed56302b1f54c82f63975541fb8887f4
chitishviligabrieli <157848399+chitishviligabrieli@users.noreply.github.com>
2024-07-18 18:00:16 +0400

    fix: structure

--- app/components/PlaylistItem/PlaylistItem.tsx
+++ app/components/PlaylistItem/PlaylistItem.tsx
@@ -1,7 +1,5 @@
 import styles from "./PlaylistItem.module.scss";
 import Icon, {IconEnum} from "@/app/components/Icon/Icon";
-import {ReactNode, useEffect, useState} from "react";
-import axios from "axios";
 
 
 interface Props {
@@ -14,45 +12,27 @@ interface Props {
 }
 
 export const PlaylistItem = (props: Props) => {
-
-    const [playlist, setPlaylist] = useState<Props[]>([]);
-
-    useEffect(() => {
-        axios.get('https://fakestoreapi.com/products')
-            .then(res => {
-                setPlaylist(res.data)
-
-            })
-            .catch(err => console.error(err));
-    }, []);
-    console.log(playlist)
-
-
-
     return (
         <>
-            {playlist.map((item, index) => (
-                <div key={index} className={styles.playlistItem}>
-                    <div className={styles.leftSection}>
-                        <div className={styles.imageSection}>
-                            <img className={styles.icon} src={IconEnum.AUDIO} alt="audio"/>
-                            <img className={styles.image} src={item.image} alt={`album ${index}`}/>
-                        </div>
-                        <div className={styles.text}>
-                            <span>{`${item.title} - ${item.category}`}</span>
-                        </div>
+            <div className={styles.playlistItem}>
+                <div className={styles.leftSection}>
+                    <div className={styles.imageSection}>
+                        <img className={styles.icon} src={IconEnum.AUDIO} alt="audio"/>
+                        <img className={styles.image} src={props.image} alt={`album`}/>
                     </div>
-                    <div className={styles.rightSection}>
-                        <div className={styles.time}>
-                            <img src={IconEnum.CLOCK} alt={''}/>
-                            <span className={styles.text}>{(item.price/60).toFixed(2)}</span>
-                        </div>
-                        <img className={`${styles.icon} ${styles.heart}`} src={IconEnum.HEART} alt={''} />
-                        <img className={styles.dots} src={IconEnum.DOTS} alt={''}/>
+                    <div className={styles.text}>
+                        <span>{`${props.title} - ${props.category}`}</span>
                     </div>
                 </div>
-            ))}
+                <div className={styles.rightSection}>
+                    <div className={styles.time}>
+                        <img src={IconEnum.CLOCK} alt={''}/>
+                        <span className={styles.text}>{(props.price / 60).toFixed(2)}</span>
+                    </div>
+                    <img className={`${styles.icon} ${styles.heart}`} src={IconEnum.HEART} alt={''}/>
+                    <img className={styles.dots} src={IconEnum.DOTS} alt={''}/>
+                </div>
+            </div>
         </>
     )
 }
-
--- app/page.tsx
+++ app/page.tsx
@@ -3,11 +3,12 @@ import styles from "./page.module.css";
 import Navbar from "@/app/components/Navbar/Navbar";
 import {PlaylistItem} from "@/app/components/PlaylistItem/PlaylistItem";
 import {IconEnum} from "@/app/components/Icon/Icon";
+import {Plaster} from "next/dist/compiled/@next/font/dist/google";
 
 export default function Home() {
     return (
         <main className={styles.main}>
-
+    
         </main>
     );
 }
