c9e2bb3888beeb73719837e92624243f7f01e6ce
chitishviligabrieli <157848399+chitishviligabrieli@users.noreply.github.com>
2024-07-25 15:41:56 +0400

    refactore:Buttons

--- app/components/Hero/Hero.tsx
+++ app/components/Hero/Hero.tsx
@@ -2,34 +2,36 @@
 
 import styles from './Hero.module.scss';
 import Navbar from '../Navbar/Navbar';
-import Button from '../Button/Button';
+import {Button} from "@/app/components/Button/Button";
+import {IconEnum} from "@/app/utils/Icon/Icon";
 
 const Hero = () => {
-    
 
-    return(
+
+    return (
         <section className={styles.section}>
             <Navbar/>
             <div className={styles.content}>
                 <div className={styles.container}>
                     <div className={styles.wrapper}>
-                        <h2 className={styles.heroHeading}>Arianna Grande :  Thank You Next</h2>
+                        <h2 className={styles.heroHeading}>Arianna Grande : Thank You Next</h2>
                         <p className={styles.heroText}>A self-empowerment anthem reflecting on
-                        past relationships with gratitude and moving 
-                        forward with confidence.
+                            past relationships with gratitude and moving
+                            forward with confidence.
                         </p>
-
-                        <Button className='play' title='Play'></Button>
-
+                        <Button bg={'none'} title={'Shuffle'}
+                                icon={"SHUFFLE"}
+                                hoverIcon={"SHUFFLEHOVER"}
+                                activeIcon={'SHUFFLE'}
+                        ></Button>
                     </div>
-                    <img src={'./images/ariana.png'} alt="img" width={265} height={392} />
-                    
+                    <img src={'./images/ariana.png'} alt="img" width={265} height={392}/>
                 </div>
-
             </div>
-
         </section>
     )
 }
 
 export default Hero;
+
+
