fcbfa6be9d0a2bfc7de649826ce88a917c5c1633
bokakajilashvili <128229402+bokakajilashvili@users.noreply.github.com>
2024-07-31 15:31:01 +0400

    forCheckout

--- app/(user)/page.tsx
+++ app/(user)/page.tsx
@@ -5,6 +5,7 @@ import { artists, playlistData, popularAlbum } from "../data/CarouselData";
 import styles from "./page.module.css";
 import {IconEnum} from "@/app/utils/Icon/Icon";
 import Hero from "@/app/components/Hero/Hero";
+import ArtistPage from "../pages/ArtistPage/ArtistPage";
 
 export default function Home() {
   const [isClient, setIsClient] = useState(false);
@@ -16,10 +17,7 @@ export default function Home() {
     <main className={styles.main} style={{ backgroundColor: "black" }}>
       {isClient && (
         <>
-          <Hero/>
-          <Slider data={popularAlbum} title="Popular Album" />
-          <Slider data={artists} title="Artists" />
-          <Slider data={playlistData} title="My Playlists" />
+          <ArtistPage/>
         </>
       )}
     </main>
