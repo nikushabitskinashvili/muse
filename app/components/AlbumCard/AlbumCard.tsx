8e1018a2307ad3d7d3d5a27a033298c7f10cdf93
chitishviligabrieli <157848399+chitishviligabrieli@users.noreply.github.com>
2024-08-20 19:41:05 +0400

    refactore: paddings

--- app/components/AlbumCard/AlbumCard.tsx
+++ app/components/AlbumCard/AlbumCard.tsx
@@ -2,20 +2,9 @@
 import styles from "./AlbumCard.module.scss";
 import Link from "next/link";
 
-
-// export interface Album {
-//     id?: number;
-//     item: string;
-//     title?: string;
-//     image?: string;
-//     name?: string;
-//     className?: string;
-//     onClick?: void;
-// }
-
 const AlbumCard = ({item}: {item: any }) => {
     return (
-            <Link className={styles.albumCard} href={`album/${item.id}`}>
+            <Link className={styles.albumCard} href={`albums/${item.id}`}>
                 <div className={styles.albumCard}>
                     <img className={styles.albumImg} src={item.img} alt={item.title} />
                     <div className={styles.albumName}>
