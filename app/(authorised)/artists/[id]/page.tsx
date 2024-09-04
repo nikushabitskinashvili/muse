f6ffd5227aa8cf3b433f62c500c7286cb25563a2
bokakajilashvili <128229402+bokakajilashvili@users.noreply.github.com>
2024-09-04 17:15:23 +0400

    refactor/FBLAN-48/artistPage

--- app/(authorised)/artists/[id]/page.tsx
+++ app/(authorised)/artists/[id]/page.tsx
@@ -1,19 +1,19 @@
-'use client';
-import { MusicWrapper } from '@/app/components/musicWrapper/musicWrapper';
-import styles from './ArtistPage.module.scss'
+"use client";
+import { MusicWrapper } from "@/app/components/musicWrapper/musicWrapper";
+import styles from "./ArtistPage.module.scss";
 import { ArtistHero } from "@/app/components/ArtistHero/ArtistHero";
 
-export default function Artist  ()  {
-    return(
-        <div className={styles.main}>
-            <ArtistHero artistName="Lana Del Rey" imgSrc="./images/ArtistHero.png" songsCount={56000}/>
-            <div className={styles.MusicWrapper}>
-                <MusicWrapper/>
-            </div>
-
-        
-
-        </div>
-    )
-
+export default function Artist() {
+  return (
+    <div className={styles.main}>
+      <ArtistHero
+        artistName="Lana Del Rey"
+        imgSrc="./images/ArtistHero.png"
+        songsCount={56000}
+      />
+      <div className={styles.MusicWrapper}>
+        <MusicWrapper />
+      </div>
+    </div>
+  );
 }
--- app/(authorised)/artists/page.tsx
+++ app/(authorised)/artists/page.tsx
@@ -1,24 +1,22 @@
-"use client"
+"use client";
 
-import styles from './ArtistPage.module.scss';
-import Navbar from '../../components/Navbar/Navbar';
-import { ArtistCard } from '../../components/ArtistCard/ArtistCard';
-import { artists } from '../../data/CarouselData';
-import { title } from 'process';
-export default  function artist () {
+import styles from "./ArtistPage.module.scss";
+import Navbar from "../../components/Navbar/Navbar";
+import { ArtistCard } from "../../components/ArtistCard/ArtistCard";
+import { artists } from "../../data/CarouselData";
+import { title } from "process";
 
-    return(
-        <main className={styles.main}>
-            <div className={styles.container}>
-                <span className={styles.title}>The Artists</span>
-
-                <div className={styles.wrapper}>
-                    
-                    {artists.map(item => (
-                        <ArtistCard title={title} item = {item}/>
-                    ))}
-                </div>
-            </div>
-        </main>
-    )
+export default function artist() {
+  return (
+    <main className={styles.main}>
+      <div className={styles.container}>
+        <span className={styles.title}>The Artists</span>
+        <div className={styles.wrapper}>
+          {artists.map((item) => (
+            <ArtistCard title={title} item={item} />
+          ))}
+        </div>
+      </div>
+    </main>
+  );
 }
