f85f0fb679c015314553051ca82bb289ce9c98de
nikushabitskinashvili <130066334+nikushabitskinashvili@users.noreply.github.com>
2024-09-05 16:16:59 +0400

    fix: fix aws errors

--- app/array.tsx
+++ app/array.tsx
@@ -1,22 +1,27 @@
+import { Song } from '@/app/Interfaces/Interfaces';
 
-const songs = [
+
+const songs : Song[] = [
     {
         src: "/images/barry.jpg",
         music: "Love making music",
         artist: "Barry White",
-        audioSrc: "/musics/Barry White - Love Making Music (1).mp3"
+        audioSrc: "/musics/Barry White - Love Making Music (1).mp3",
+        duration: 0
     },
     {
         src: "/images/barry.jpg",
         music: "Let the music play",
         artist: "Barry white",
-        audioSrc: "/musics/Barry White - Let The Music Play (Official Music Video) (1).mp3"
+        audioSrc: "/musics/Barry White - Let The Music Play (Official Music Video) (1).mp3",
+        duration: 0
     },
     {
         src: "/images/mefe.jpg",
         music: 'me shentan moval',
         artist: "mefe",
-        audioSrc: "/musics/ირაკლი ჩარკვიანი - მე შენთან მოვალ.mp3"
+        audioSrc: "/musics/ირაკლი ჩარკვიანი - მე შენთან მოვალ.mp3",
+        duration: 0
     }
 ]
 
