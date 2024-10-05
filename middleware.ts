eee1fe926aaa8de40a3963543d0525ae016bf2ff
Irakli Pankvelashvili <irakli.pankvelashvili@gmail.com>
2024-10-05 13:02:15 +0400

    modify middlware

--- middleware.ts
+++ middleware.ts
@@ -1,15 +1,23 @@
 import { cookies } from "next/headers";
 import { NextRequest, NextResponse } from "next/server";
-import { jwtVerify } from "jose"; 
 import { AUTH_COOKIE_KEY } from "./app/constant";
 
-async function verifyToken(token: string) {
-  const secret = new TextEncoder().encode("daculiparoli"); 
+function decodeToken(token:string) {
   try {
-    const { payload } = await jwtVerify(token, secret);
-    return payload;
+    const parts = token.split(".");
+    
+    if (parts.length !== 3) {
+      throw new Error("Invalid token format");
+    }
+
+    const payload = parts[1];
+    
+    const base64 = payload.replace(/-/g, '+').replace(/_/g, '/');
+    const decodedPayload = JSON.parse(atob(base64));
+    
+    return decodedPayload; 
   } catch (error) {
-    console.error("Invalid token:", error);
+    console.error("Failed to decode token:", error);
     return null;
   }
 }
@@ -23,24 +31,30 @@ export async function middleware(request: NextRequest) {
     return NextResponse.redirect(new URL("/auth/login", request.url));
   }
 
-  if (cookie?.value && (pathname.startsWith("/auth/login") || pathname.startsWith("/auth/signup"))) {
+  if (cookie?.value && (pathname.startsWith("/auth/login") || pathname.startsWith("/auth/signup") || pathname.startsWith("/auth/login?error=blocked"))) {
     return NextResponse.redirect(new URL("/", request.url));
   }
 
   if (cookie?.value) {
     const token = cookie.value;
-    const decodedToken = await verifyToken(token);
+    const decodedToken = decodeToken(token);
+
 
     if (decodedToken && decodedToken.blocked) {
       const response = NextResponse.redirect(new URL("/auth/login?error=blocked", request.url));
-      response.cookies.set(AUTH_COOKIE_KEY, '', { expires: new Date(0), path: '/' });
-      return response;
+
+
+      response.cookies.set(AUTH_COOKIE_KEY, "", {
+        path: "/", 
+        maxAge: -1, 
+      });
+
+      return response; 
     }
   }
 
   return NextResponse.next();
 }
-
 export const config = {
   matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
 };
